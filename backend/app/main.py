e46357f762f6d0ddfc5c8816996d885129d8640a
rianto42 <rianto.utomo@student.uts.edu.au>
2026-02-03 16:58:16 +0700

    open origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -71,7 +71,7 @@ app = FastAPI(title="ProMan API", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://project-tracker-backend-jz5x.onrender.com"],
+    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173","https://project-tracker-backend-jz5x.onrender.com","https://project-tracker-ly4l.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
